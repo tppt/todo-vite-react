b8c51501fc539ebfb1a6ebe48cc39895e482c287
Thomas Travis <thomastravis@Thomass-MBP.home>
2023-07-20 19:58:27 -0400

    add prettier, format files, handle onkeyup and onblur when editing todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import Todo from './components/todo'
-import { TextField, Button, Checkbox } from '@mui/material';
-import { nanoid } from 'nanoid';
-import DeleteIcon from '@mui/icons-material/Delete';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Todo from "./components/todo";
+import { TextField, Button, Checkbox } from "@mui/material";
+import { nanoid } from "nanoid";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function App() {
-  let myStoredTodos = localStorage.getItem('todos');
-  let myAllChecked = localStorage.getItem('allChecked');
-  const [todos, setTodos] = useState(myStoredTodos ? JSON.parse(myStoredTodos) : []); 
-  const [todoEntry, setTodoEntry] = useState('');
-  const [allChecked, setAllChecked] = useState(myAllChecked ? JSON.parse(myAllChecked) : false);
-  
+  let myStoredTodos = localStorage.getItem("todos");
+  let myAllChecked = localStorage.getItem("allChecked");
+  const [todos, setTodos] = useState(
+    myStoredTodos ? JSON.parse(myStoredTodos) : [],
+  );
+  const [todoEntry, setTodoEntry] = useState("");
+  const [allChecked, setAllChecked] = useState(
+    myAllChecked ? JSON.parse(myAllChecked) : false,
+  );
+
   useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos));
+    localStorage.setItem("todos", JSON.stringify(todos));
     if (todos.length === 0) setAllChecked(false);
   }, [todos]);
 
   useEffect(() => {
-    localStorage.setItem('allChecked', JSON.stringify(allChecked));
+    localStorage.setItem("allChecked", JSON.stringify(allChecked));
   }, [allChecked]);
 
   function handleTodoEntry(event) {
@@ -28,14 +32,14 @@ function App() {
   function handleSubmit(event) {
     event.preventDefault();
     if (!todoEntry) return;
-    setTodos([ { id: nanoid(), checked: false, value: todoEntry }, ...todos ])
-    setTodoEntry('');
+    setTodos([{ id: nanoid(), checked: false, value: todoEntry }, ...todos]);
+    setTodoEntry("");
   }
 
   function handleCheckbox(id) {
     const updatedTodos = todos.map((todo) => {
       if (todo.id === id) {
-        return { ...todo, checked: !todo.checked }
+        return { ...todo, checked: !todo.checked };
       }
       return todo;
     });
@@ -45,10 +49,11 @@ function App() {
   function handleValueChange(id, value) {
     const updatedTodos = todos.map((todo) => {
       if (todo.id === id) {
-        return { ...todo, value: value }
+        return { ...todo, value: value };
       }
       return todo;
     });
+
     setTodos(updatedTodos);
   }
 
@@ -66,29 +71,80 @@ function App() {
     setTodos(updatedTodos);
   }
 
+  function handleTodoKeyUp(event, id, value) {
+    if (event.code === "Enter") {
+      if (!value) {
+        const updatedTodos = todos.filter((todo) => todo.id !== id);
+        setTodos(updatedTodos);
+      }
+      event.target.blur();
+      return;
+    }
+  }
+
+  function handleBlur(id, value) {
+    if (!value) {
+      const updatedTodos = todos.filter((todo) => todo.id !== id);
+      setTodos(updatedTodos);
+    }
+  }
+
   return (
     <main>
       <header>
         <h1>{`//TODO`}</h1>
         <form onSubmit={handleSubmit}>
-          <TextField variant="standard" value={todoEntry} onChange={(event) => handleTodoEntry(event)} />
-          <Button id="add-button" type="submit" variant="contained" disabled={!todoEntry.length}>Add</Button>
-          <Button id="delete-button" variant="contained" disabled={!todos.some((todo) => todo.checked)} onClick={() => handleDelete()}><DeleteIcon /></Button>
+          <TextField
+            variant="standard"
+            value={todoEntry}
+            onChange={(event) => handleTodoEntry(event)}
+          />
+          <Button
+            id="add-button"
+            type="submit"
+            variant="contained"
+            disabled={!todoEntry.length}
+          >
+            Add
+          </Button>
+          <Button
+            id="delete-button"
+            variant="contained"
+            disabled={!todos.some((todo) => todo.checked)}
+            onClick={() => handleDelete()}
+          >
+            <DeleteIcon />
+          </Button>
         </form>
       </header>
       <div id="check-all-div">
-        <Checkbox id="check-all" disabled={todos.length === 0} checked={allChecked} onChange={(event) => handleCheckAll(event)} />
-        <label htmlFor="check-all" className={!todos.length && 'disabled-text'}>{allChecked ? `Uncheck All` : `Check All`}</label>
+        <Checkbox
+          id="check-all"
+          disabled={todos.length === 0}
+          checked={allChecked}
+          onChange={(event) => handleCheckAll(event)}
+        />
+        <label htmlFor="check-all" className={!todos.length && "disabled-text"}>
+          {allChecked ? `Uncheck All` : `Check All`}
+        </label>
       </div>
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>
-            <Todo id={todo.id} checked={todo.checked} handleCheckbox={handleCheckbox} value={todo.value} handleValueChange={handleValueChange} />
+            <Todo
+              id={todo.id}
+              checked={todo.checked}
+              handleCheckbox={handleCheckbox}
+              value={todo.value}
+              handleValueChange={handleValueChange}
+              handleTodoKeyUp={handleTodoKeyUp}
+              handleBlur={handleBlur}
+            />
           </li>
         ))}
       </ul>
     </main>
-  )
+  );
 }
 
 export default App;
--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,12 +1,28 @@
-import PropTypes from 'prop-types';
-import { Checkbox, TextField } from '@mui/material';
-import './todo.css';
+import PropTypes from "prop-types";
+import { Checkbox, TextField } from "@mui/material";
+import "./todo.css";
 
-export default function Todo({ id, checked, handleCheckbox, value, handleValueChange }) {
+export default function Todo({
+  id,
+  checked,
+  handleCheckbox,
+  value,
+  handleValueChange,
+  handleTodoKeyUp,
+  handleBlur,
+}) {
   return (
     <div>
       <Checkbox checked={checked} onChange={() => handleCheckbox(id)} />
-      <TextField className={`${checked && 'strikethrough'}`} value={value} disabled={checked} variant="standard" onChange={(event) => handleValueChange(id, event.target.value)} />
+      <TextField
+        className={`${checked && "strikethrough"}`}
+        value={value}
+        disabled={checked}
+        variant="standard"
+        onChange={(event) => handleValueChange(id, event.target.value)}
+        onKeyUp={(event) => handleTodoKeyUp(event, id, value)}
+        onBlur={() => handleBlur(id, value)}
+      />
     </div>
   );
 }
@@ -16,5 +32,7 @@ Todo.propTypes = {
   checked: PropTypes.bool,
   value: PropTypes.string,
   handleCheckbox: PropTypes.func,
-  handleValueChange: PropTypes.func
+  handleValueChange: PropTypes.func,
+  handleTodoKeyUp: PropTypes.func,
+  handleBlur: PropTypes.func,
 };
